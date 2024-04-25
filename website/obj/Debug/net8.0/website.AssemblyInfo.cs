90b4894fd6d3a73a6f1a9c5b3a3dae66b341daf7
mvuksan544 <mvuksan544@gmail.com>
2024-04-25 21:28:24 +0200

    Backend changes

--- website/obj/Debug/net8.0/website.AssemblyInfo.cs
+++ website/obj/Debug/net8.0/website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b1b66b26e6e2f9704fada8546978191c2e8036")]
 [assembly: System.Reflection.AssemblyProductAttribute("website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
